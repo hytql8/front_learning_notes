dbe858dd0f67e94d5939345b14d336e5d47f1ffe
hytql8 <1711034994@qq.com>
2024-04-16 18:32:31 +0800

    发布订阅模式

---
+++ publish-subscription/index.js
@@ -0,0 +1,59 @@
+const Pulisher = function () {};
+
+const pulisher = () => {
+  const pub = new Pulisher();
+  pub.__proto__.events = new Map();
+  // 订阅事件
+  pub.__proto__.on = function (event, callback) {
+    const weakSet = this.events.get(event);
+    if (!weakSet) {
+      const effectSet = new Set();
+      effectSet.add(callback);
+      this.events.set(event, effectSet);
+    } else {
+      const effectSet = weakSet.add(callback);
+      effectSet.add(callback);
+      weakSet.set(event, effectSet);
+    }
+  };
+  // 只订阅一次
+  pub.__proto__.once = function (event, callback) {
+    const fn = (...args) => {
+      callback(...args);
+      this.off(event, fn);
+    };
+    this.on(event, fn);
+  };
+  // 发布事件
+  pub.__proto__.emit = function (event, ...args) {
+    const weakSet = this.events.get(event);
+    if (!weakSet) return;
+    weakSet.forEach((event) => event(...args));
+  };
+  // 取消订阅
+  pub.__proto__.off = function (event, callback) {
+    const weakSet = this.events.get(event);
+    if (!weakSet) return;
+    if (callback) {
+      weakSet.delete(callback);
+    } else {
+      weakSet.clear();
+    }
+  };
+  return pub;
+};
+
+export const run = () => {
+  const res = pulisher();
+  res.on("event", (...args) => {
+    console.log("监听到event的消息:", ...args);
+  });
+
+  setInterval(() => {
+    res.emit("event", "hello world", new Date());
+  }, 1000);
+
+  setInterval(() => {
+    res.off("event");
+  }, 3000);
+};
